b300cb04e0e5bc4bc79102aa2f1864d8e1daf292
Arvind Sharma <mail.arvind.s@gmail.com>
2022-02-12 22:33:26 +0000

    added the server boilerplate template

--- my-app/src/helper_functions.js
+++ my-app/src/helper_functions.js
@@ -2,6 +2,7 @@ const units_needed_per_day = 12;
 const sunshine_hours_per_day = 3.3;
 const crystaline_module = true;
 //const crystaline_module = false;
+const site_area = 15;
 
 const power_needed_by_load = function (units, hours) {
 
@@ -33,7 +34,7 @@ const operations_loss = function (crystaline_module) {
   }
   return 19.25;
 }
-console.log(operations_loss())
+console.log(operations_loss(crystaline_module))
 
 const power_plant_capacity = function () {
   const loss = operations_loss() / 100;
@@ -46,6 +47,7 @@ console.log("Plant capacity: ",power_plant_capacity())
 const avg = function (a, b) {
   return (a + b) / 2;
 }
+
 //Inverter Fake data
 const inverters = {
   1: {'model name': 'model1', Vmax: 450, Vmpp: avg(120, 150), Imax: 12, Efficiency: 97},
@@ -62,18 +64,42 @@ const inverter_estimated_rating = function() {
   const safety_factor = assumption / 100
   return dc_power_input_to_inverter() / (1 - safety_factor);
 }
+console.log(inverter_estimated_rating());
 
 
-const selected_modules = 1;
-const total_modules = function() {
-  return power_plant_capacity() / selected_modules[Vmp]
-}
-
-const selecter_inverters_from_db = function () {
-  inverters.map(inverter => {inverter.Vmax > inverter_estimated_rating() && (inverter.Vmax < (inverter_estimated_rating() * 1.1)) })
+const selected_modules = function() {
   
+  return ;
 }
 
-const modules_in_string = function () {
-  
+const total_modules = function(selected_module) {
+  return power_plant_capacity() / selected_module[Vm]
 }
+
+const combo = function() {
+
+  //gives me set of modules 
+  const selected_modules_from_db = modules.map(module => {
+    (module.length * module.breadth) * total_modules(module) < site_area
+  });
+  //gives me set of inverters I need
+  const selected_inverters_from_db = function () {
+    return inverters.map(inverter => {inverter.Vmax > inverter_estimated_rating() && (inverter.Vmax < (inverter_estimated_rating() * 1.15)) })
+    
+  }
+  
+  const modules_in_string = function () {
+    selected_inverters[Vmpp] / selected_module[Vm]
+  }
+
+
+}
+
+//area needed = power_needed_by_load * 95 sq mt
+//number of panels = power_needed_by_load x 3
+//const module_covered_area = module.length * modules_in_string * module.breadth * total_strings * 1.5;
+// pseudo code herE:
+// select all modules.----- no reduce the number of choice ---- by area? then build a counter table that matches the area of module with the area of site. 
+
+
+
