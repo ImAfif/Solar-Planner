6d15cd4231c3238cfb646b70bd887ef31378995f
Arvind Sharma <mail.arvind.s@gmail.com>
2022-02-13 23:03:53 +0000

    bug fixed for module selection function and inverter selection functions

--- my-app/src/helper_functions.js
+++ my-app/src/helper_functions.js
@@ -1,4 +1,4 @@
-const units_needed_per_day = 12;
+const units_needed_per_day = 15;
 const sunshine_hours_per_day = 3.3;
 const crystaline_module = true;
 //const crystaline_module = false;
@@ -51,14 +51,14 @@ const avg = function (a, b) {
 //Inverter Fake data
 const inverters = {
   1: {'model name': 'model1', Vmax: 600, Vmpp: avg(400, 800), Imax: 35, Efficiency: 98.2, Pac: 17, Price: 1500},//Pac in kva
-  2: {'model name': 'model2', Vmax: 400, Vmpp: avg(80, 100), Imax: 10, Efficiency: 98, Price: 1600},
-  1: {'model name': 'model3', Vmax: 600, Vmpp: avg(100, 160), Imax: 75, Efficiency: 97.5, Pac: 10, Price: 1100}
+  2: {'model name': 'model2', Vmax: 400, Vmpp: avg(80, 100), Imax: 10, Efficiency: 98, Pac: 40, Price: 1600},
+  3: {'model name': 'model3', Vmax: 600, Vmpp: avg(100, 160), Imax: 75, Efficiency: 97.5, Pac: 10, Price: 1100}
 }
 //modules fake data
 const modules = {
   1: {'model name': 'model1', Pm: 210, Voc: 41.59, Isc: 7.13, Vm: 33.81, Im: 6.21, Price: 100 },
   2: {'model name': 'model2', Pm: 350, Voc: 39, Isc: 8, Vm: 32, Im: 10, Price: 150 },
-  3: {'model name': 'model3', Pm: 145, Voc: 22.4, Isc: 8.5, Vm: 18.35, Im: 7.9, Price: 85 },
+  3: {'model name': 'model3', Pm: 151, Voc: 22.4, Isc: 8.5, Vm: 18.35, Im: 7.9, Price: 85 },
   4: {'model name': 'model4', Pm: 240, Voc: 37.38, Isc: 8.45, Vm: 30.60, Im: 7.85, Price: 110 },
   5: {'model name': 'model5', Pm: 150, Voc: 21.4, Isc: 7.5, Vm: 19.35, Im: 6.9, Price: 90 },
 }
@@ -70,47 +70,68 @@ const inverter_estimated_rating = function() {
 }
 console.log(inverter_estimated_rating());
 
-const input_range = 3;
+const input_range = 1;
 
 //gives me set of modules based user input
-const selected_modules_from_db = function () { 
+const selected_modules_from_db = function (input_range) { 
   if (input_range === 1) {
-    return modules.map(module => {module[Vm] < 200 && module[Vm] > 150})
+    return Object.values(modules).filter(module => 
+      module.Pm < 200 && module.Pm > 150)
   }
-  if (input_range === 2)
-  return modules.map(module => {module[Vm] < 250 && module[Vm] > 200}) 
-  if (input_range === 3)
-  return modules.map(module => {module[Vm] < 300 && module[Vm] > 250})
-  if (input_range === 4)
-  return modules.map(module => {module[Vm] < 350 && module[Vm] > 300})
-  if (input_range === 5)
-  return modules.map(module => {module[Vm] < 400 && module[Vm] > 350})
+  if (input_range === 2) {
+    return Object.values(modules).filter(module => module.Pm < 250 && module.Pm > 200) 
+  }
+  if (input_range === 3) {
+  
+    return Object.values(modules).filter(module => module.Pm < 300 && module.Pm > 250)
+  }
+  if (input_range === 4) {
+    return Object.values(modules).filter(module => module.Pm < 350 && module.Pm > 300)
+
+  }
+  if (input_range === 5) {
+    return Object.values(modules).filter(module => module.Pm < 400 && module.Pm > 350)
+
+  }
 }
+
+console.log(selected_modules_from_db(input_range))
  // module1: modules[3], module2: modules[5]} //from input range
  
  //gives me set of inverters I need
  const selected_inverters_from_db = function () {
-   return inverters.map(inverter => {inverter.Vmax > inverter_estimated_rating() && (inverter.Vmax < (inverter_estimated_rating() * 1.15)) })
+   return Object.values(inverters).filter(inverter => {
+    return ( 
+      inverter.Pac > inverter_estimated_rating() && 
+      inverter.Pac < (inverter_estimated_rating() * 1.15)
+    ) 
+    })
  }
+ console.log(selected_inverters_from_db())
 
 const total_modules = function(selected_module) {
-  return power_plant_capacity() / selected_module[Vm]
+  return power_plant_capacity() / selected_module.Pm
 }
+console.log('total modules: ', total_modules(modules[3]))
 
 const modules_in_string = function (selected_module, selected_inverters ) {
-  return selected_inverters[Vmpp] / selected_module[Vm]
+  return selected_inverters.Vmpp / selected_module.Vm
 }
+console.log(modules_in_string(modules[3], inverters[1]))
 
 const combination_compatibility = function (module, inverter) {
   
-  if (modules_in_string(module, inverter) * module[Vm] < inverter[Vmpp])
+  if (modules_in_string(module, inverter) * module.Vm < inverter.Vmpp)
   return true //if changed Vmp is in range of the selected inverter's Vmp then fine else not compatible
   return false
 }
 
+console.log(combination_compatibility(modules[3], inverters[1]))
+
 const total_strings = function(module, inverter) {
   return total_modules(module) / modules_in_string(module, inverter)
 }
+console.log(total_strings(modules[3], inverters[1]))
 
 const combo = function(modules, inverters) {
   
@@ -123,15 +144,10 @@ const combo = function(modules, inverters) {
   return { module, inverter, all_number_of_modules, series_modules, modules_in_parallel };
 }))
 
-  // const selected_modules_from_db = modules.map(module => {
-  //   (module.length * module.breadth) * total_modules(module) < site_area
-  // });
-  
-
 
 }
 
-combo(selected_modules_from_db, selected_inverters_from_db)
+console.log('combo: ',combo(selected_modules_from_db(input_range), selected_inverters_from_db()))
 
 //area needed = power_needed_by_load * 95 sq mt
 //number of panels = power_needed_by_load x 3
