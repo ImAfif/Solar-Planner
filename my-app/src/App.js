e1d74ff6db48bb38defad4cafa15f70d617a6629
Arvind Sharma <mail.arvind.s@gmail.com>
2022-02-16 21:25:01 +0000

    add axios to comunicate with the db in backend and implement useEffect and useState, not yet tested

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 // import Navigation from "./components/Navigation";
 // import Showcase from "./components/Showcase";
@@ -8,11 +8,39 @@ import Home from "./components/Home";
 import About from "./components/About";
 //import YoutubeEmbed from "./components/YoutubeEmbed";
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
+import axios from "axios";
+
 
 
 function App() {
+
+
+  const [state, setState] = useState({
+    users: [],
+    modules: [],
+    inverters: [],
+    combos: {}
+  })
+
+  useEffect(() => {
+    Promise.all([
+      axios.get(`../back-end/db/schema/01_users.sql`),
+      axios.get(`../back-end/db/schema/02_solar_panel.sql`),
+      axios.get(`../back-end/db/schema/03_inverters.sql`),
+      axios.get(`../back-end/db/schema/04_grid_options.sql`)
+    ]).then(response => {
+      setState(prev => ({ ...prev, 
+        users: response[0].data, 
+        modules: response[1].data, 
+        inverters: response[2].data, 
+        combos: response[3].data
+      }))
+    });
+  }, []);
+
   return (
     <div className="App">
+
       <Router>
         
         <Routes>
